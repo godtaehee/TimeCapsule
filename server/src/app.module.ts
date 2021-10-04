dd8ee380020f62bac2c5af426b5408da66a1f8d5
taehee <wonderfulhuman@naver.com>
2021-10-05 01:34:14 +0900

    [Server] Feat: User Module추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({})],
+  imports: [TypeOrmModule.forRoot({}), UsersModule],
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
