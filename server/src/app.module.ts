cd25bf23b4a8f44a63a9656d17528b0eebfcd76f
taehee <wonderfulhuman@naver.com>
2021-10-04 23:37:25 +0900

    [Server] Feat: User Controller 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [TypeOrmModule.forRoot({})],
-  controllers: [AppController],
+  controllers: [AppController, UsersController],
   providers: [AppService],
 })
 export class AppModule {}
