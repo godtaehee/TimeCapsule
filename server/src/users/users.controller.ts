8837a2076281a96af56864ebd4c665aed738c4ce
taehee <wonderfulhuman@naver.com>
2021-10-05 03:18:33 +0900

    [Server] Feat: UserController에 SignUp 메서드 추가

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,4 +1,10 @@
 import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private userService: UsersService) {}
+  signUp(signUpUserInfo: any) {
+    return this.userService.signUp(signUpUserInfo);
+  }
+}
