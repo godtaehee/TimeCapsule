a277834fb8d34c304d01532c59259f7b2f1659a8
taehee <wonderfulhuman@naver.com>
2021-10-05 02:29:19 +0900

    [Server] Test: it should return JSON which have success status and userId field if Sign-up is Success

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import * as faker from 'faker';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -14,5 +16,29 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+});
+
+describe('Sign-up in UserController', () => {
+  let usersController: UsersController;
+  let usersService: UsersService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+    }).compile();
+
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
+  });
+
+  it('should return JSON which have success status and userId field if Sign-up is Success', () => {
+    const userInfo = {};
+    const successSignUp = {
+      success: true,
+      userId: faker.random.alphaNumeric(),
+    };
+    usersService.signUp = jest.fn().mockResolvedValue(successSignUp);
+    const result = usersController.signUp(userInfo as any);
+    expect(result).toBe(successSignUp);
   });
 });
