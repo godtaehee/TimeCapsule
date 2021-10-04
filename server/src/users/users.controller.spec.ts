9f0347e34a611b9059dc224e550fbea4c949fb1b
taehee <wonderfulhuman@naver.com>
2021-10-05 03:27:25 +0900

    [Server] Test: Describe 안에 Describe를 Nested한 형식으로 Test코드 구조를 바꾸었다. 그래야 TestingModule에서 가져온 Dependency들을 한번만 Compile하여 재사용 할수 있다.

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -4,41 +4,33 @@ import * as faker from 'faker';
 import { UsersService } from './users.service';
 
 describe('UsersController', () => {
-  let controller: UsersController;
-
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [UsersController],
-    }).compile();
-
-    controller = module.get<UsersController>(UsersController);
-  });
-
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
-});
-
-describe('Sign-up in UserController', () => {
   let usersController: UsersController;
   let usersService: UsersService;
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [UsersService],
     }).compile();
 
     usersController = module.get<UsersController>(UsersController);
     usersService = module.get<UsersService>(UsersService);
   });
 
-  it('should return JSON which have success status and userId field if Sign-up is Success', () => {
-    const userInfo = {};
-    const successSignUp = {
-      success: true,
-      userId: faker.random.alphaNumeric(),
-    };
-    usersService.signUp = jest.fn().mockResolvedValue(successSignUp);
-    const result = usersController.signUp(userInfo as any);
-    expect(result).toBe(successSignUp);
+  it('should be defined', () => {
+    expect(usersController).toBeDefined();
+  });
+
+  describe('Sign-up in UserController', () => {
+    it('should return JSON which have success status and userId field if Sign-up is Success', async () => {
+      const userInfo = {};
+      const successSignUp = {
+        success: true,
+        userId: faker.random.alphaNumeric(),
+      };
+      usersService.signUp = jest.fn().mockResolvedValue(successSignUp);
+      const result = await usersController.signUp(userInfo as any);
+      expect(result).toBe(successSignUp);
+    });
   });
 });
