7318722245b710d53a172f961faaab2df3ea81ac
taehee <wonderfulhuman@naver.com>
2021-10-05 03:19:15 +0900

    [Server] Feat: UserService에 SignUp 메서드 추가

--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  async signUp(signUpUserInfo: any) {
+    throw new Error('Method not implemented.');
+  }
+}
