9c9f41f01a6fd01ed42fbdb901c297af80ed3217
taehee <wonderfulhuman@naver.com>
2021-10-05 18:05:16 +0900

    [Server] Docs: app.module에 필요없는 Module Import 제거

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { typeORMConfig } from './config/typeorm.config';
 
 @Module({
   imports: [TypeOrmModule.forRoot(typeORMConfig), UsersModule],
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
