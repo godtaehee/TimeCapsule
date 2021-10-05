0f0af158e910bbda416b06a726fc28a57cab7d58
taehee <wonderfulhuman@naver.com>
2021-10-05 23:40:10 +0900

    [Server] Test: UserService Sign-up should return JSON which have success status and userId field if Sign-up is Success

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -2,7 +2,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
 import * as faker from 'faker';
 import { UsersService } from './users.service';
-
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from './users.entity';
+class MockUsersRepository {
+  #successSignUpDate = {
+    success: true,
+    userId: 1,
+  };
+  save(userData: any) {
+    return this.#successSignUpDate;
+  }
+}
 describe('UsersController', () => {
   let usersController: UsersController;
   let usersService: UsersService;
@@ -10,7 +20,13 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(Users),
+          useClass: MockUsersRepository,
+        },
+      ],
     }).compile();
 
     usersController = module.get<UsersController>(UsersController);
--- server/src/users/users.service.spec.ts
+++ server/src/users/users.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import * as faker from 'faker';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from './users.entity';
+class MockUsersRepository {
+  #userData = [{ id: 1, email: 'godtaeheedev@gmail.com', password: 123 }];
+  save(requestUserData: any) {
+    const data = this.#userData.find((v) => v.email === requestUserData.email);
+    if (data) return { success: false };
+    return {
+      success: true,
+      userId: 2,
+    };
+  }
+}
 
 describe('UsersService', () => {
-  let service: UsersService;
-
+  let usersService: UsersService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(Users),
+          useClass: MockUsersRepository,
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(usersService).toBeDefined();
+  });
+
+  describe('Sign-up in UserService', () => {
+    it('should return JSON which have success status and userId field if Sign-up is Success', () => {
+      const userData = {
+        email: 'wonderfulhuman@naver.com',
+        password: 'srch0382',
+      };
+      expect(usersService.signUp(userData as any)).resolves.toStrictEqual({
+        success: true,
+        userId: 2,
+      });
+    });
   });
 });
