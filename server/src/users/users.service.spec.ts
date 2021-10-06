98b677b75075cded467199c545f81e02276211d1
taehee <wonderfulhuman@naver.com>
2021-10-06 18:36:41 +0900

    [Server] Refactor: 안쓰는 import 정리

--- server/src/users/users.service.spec.ts
+++ server/src/users/users.service.spec.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
-import * as faker from 'faker';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { Users } from './users.entity';
 class MockUsersRepository {
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from './users.entity';
-import { UsersRepository } from './users.repository';
 import { Repository } from 'typeorm';
 
 @Injectable()
