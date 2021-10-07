1ba3ecd6351024555f331bb6dc368242a4eb1af7
taehee <wonderfulhuman@naver.com>
2021-10-07 17:13:08 +0900

    [Server] Test: it should be return userInfo

--- server/src/auth/auth.controller.spec.ts
+++ server/src/auth/auth.controller.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Users } from '../users/users.entity';
+import { SignUpDto } from '../users/dto/sign.up.dto';
+
+class MockUsersRepository {}
 
 describe('AuthController', () => {
-  let controller: AuthController;
+  let authController: AuthController;
+  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(Users),
+          useClass: MockUsersRepository,
+        },
+      ],
     }).compile();
 
-    controller = module.get<AuthController>(AuthController);
+    authController = module.get<AuthController>(AuthController);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(authController).toBeDefined();
+  });
+
+  describe('Sign-Up', () => {
+    it('should be return userInfo', async () => {
+      const signUpUserInfo: SignUpDto = {} as any;
+      const signUpResultInfo = {};
+
+      authService.signUp = jest.fn().mockResolvedValueOnce(signUpResultInfo);
+      const result = await authController.signUp(signUpUserInfo);
+      expect(result).toBe(signUpResultInfo);
+    });
   });
 });
