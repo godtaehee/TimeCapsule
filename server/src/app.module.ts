4a14419f5dd5af06a25eca294596566486258f27
taehee <wonderfulhuman@naver.com>
2021-10-07 15:55:15 +0900

    [Server] Feat: Auth Module & Provider 추가

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,11 +5,11 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { typeORMConfig } from './config/typeorm.config';
 import { CapsulesModule } from './capsules/capsules.module';
-import { ControllerService } from './repository/controller/controller.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), UsersModule, CapsulesModule],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), UsersModule, CapsulesModule, AuthModule],
   controllers: [AppController],
-  providers: [AppService, ControllerService],
+  providers: [AppService],
 })
 export class AppModule {}
