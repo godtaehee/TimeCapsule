559b4eaf53b6b19dfa70e0a67cf47b3c15de28de
taehee <wonderfulhuman@naver.com>
2021-10-07 15:31:19 +0900

    [Server] Docs: accountId에서 email로 변경하고 Unique값을 줌

--- server/src/users/users.entity.ts
+++ server/src/users/users.entity.ts
@@ -1,6 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique(['email'])
 export class Users extends BaseEntity {
   @PrimaryGeneratedColumn()
   userId: number;
@@ -9,7 +16,7 @@ export class Users extends BaseEntity {
   name: string;
 
   @Column()
-  accountId: string;
+  email: string;
 
   @Column()
   password: string;
