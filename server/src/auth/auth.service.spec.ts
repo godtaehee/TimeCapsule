15048ca496afff5a81480825948082fc95ecfe8b
taehee <wonderfulhuman@naver.com>
2021-10-09 02:03:01 +0900

    [Server] Test: auth.service.spec.ts에서 UsersRepository Mock객체로 만들어서 테스트 드디어성공

--- server/src/auth/auth.service.spec.ts
+++ server/src/auth/auth.service.spec.ts
@@ -1,15 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersRepository } from '../users/users.repository';
 
+const mockRepository = {
+  save: jest.fn(),
+  findOne: jest.fn(),
+};
 describe('AuthService', () => {
   let service: AuthService;
-
+  let repository: UsersRepository;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: UsersRepository,
+          useValue: mockRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    repository = module.get<UsersRepository>(UsersRepository);
   });
 
   it('should be defined', () => {
