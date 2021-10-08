fc57fb4ddc80b391bcb5ccb1d618c9f6b36e3d38
taehee <wonderfulhuman@naver.com>
2021-10-09 02:06:51 +0900

    [Server] Feat: Bcrypt 추가하여 회원가입

--- server/src/users/users.repository.ts
+++ server/src/users/users.repository.ts
@@ -1,5 +1,24 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { Users } from './users.entity';
+import { SignUpDto } from './dto/sign.up.dto';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(Users)
-export class UsersRepository extends Repository<Users> {}
+export class UsersRepository extends Repository<Users> {
+  async SignUp(signUpUserInfo: SignUpDto) {
+    const { email, nickname, password } = signUpUserInfo;
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({
+      email,
+      nickname,
+      password: hashedPassword,
+    });
+    try {
+      await this.save(user);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
