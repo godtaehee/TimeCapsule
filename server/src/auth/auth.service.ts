36cef43a911a7af8be13f236bf4e45f39d95015b
taehee <wonderfulhuman@naver.com>
2021-10-09 02:11:27 +0900

    [Server] Docs: UserRepository DI

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { UsersRepository } from '../users/users.repository';
+import { SignUpDto } from '../users/dto/sign.up.dto';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UsersRepository) private usersRepository: UsersRepository,
+  ) {}
+  async signUp(signUpUserInfo: SignUpDto) {
+    return this.usersRepository.SignUp(signUpUserInfo);
+  }
+}
