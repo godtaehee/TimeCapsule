3e0156fbc174db9cf2696e562b2b677bc7d3397d
taehee <wonderfulhuman@naver.com>
2021-10-11 02:54:42 +0900

    [Server] Test: UserController의 Sign-Up Test 삭제하고 AuthController로 옮김

--- server/src/users/users.controller.spec.ts
+++ server/src/users/users.controller.spec.ts
@@ -36,17 +36,4 @@ describe('UsersController', () => {
   it('should be defined', () => {
     expect(usersController).toBeDefined();
   });
-
-  describe('Sign-up in UserController', () => {
-    it('should return JSON which have success status and userId field if Sign-up is Success', async () => {
-      const userInfo = {};
-      const successSignUp = {
-        success: true,
-        userId: faker.random.alphaNumeric(),
-      };
-      usersService.signUp = jest.fn().mockResolvedValue(successSignUp);
-      const result = await usersController.signUp(userInfo as any);
-      expect(result).toBe(successSignUp);
-    });
-  });
 });
