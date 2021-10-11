0f47fff9c7ffd996389e15fca627ee2216353b21
taehee <wonderfulhuman@naver.com>
2021-10-11 11:32:49 +0900

    [Server] Test: it should generate hashed Password in auth.service.spec

--- server/src/auth/auth.service.spec.ts
+++ server/src/auth/auth.service.spec.ts
@@ -1,7 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersRepository } from '../users/users.repository';
-
+import * as faker from 'faker';
 const mockRepository = {
   save: jest.fn(),
   findOne: jest.fn(),
@@ -26,5 +26,27 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Sign-Up', () => {
+    const bcrypt = {
+      genSalt: jest
+        .fn()
+        .mockResolvedValueOnce(
+          faker.datatype.hexaDecimal(faker.datatype.number()),
+        ),
+      hash: jest
+        .fn()
+        .mockResolvedValueOnce(
+          faker.datatype.hexaDecimal(faker.datatype.number()),
+        ),
+    };
+    it('should generate hashed Password', async () => {
+      const password = faker.internet.password();
+      const salt = await bcrypt.genSalt();
+      const hashedPassword = await bcrypt.hash(password, salt);
+
+      expect(hashedPassword).toBeTruthy();
+    });
   });
 });
