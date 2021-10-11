02aaecdf7fc56b256c49a4e77965bdf74f8de981
taehee <wonderfulhuman@naver.com>
2021-10-11 12:33:34 +0900

    [Server] Feat: Port 숨기고 애플리케이션 실행시 메시지 출력

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(5000);
+  await app.listen(process.env.PORT);
+  logger.log(`http://localhost:${process.env.PORT}`);
 }
 bootstrap();
